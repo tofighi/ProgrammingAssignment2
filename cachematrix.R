fe0b077f3bfb7b255f4657f739c373a7198f3906
tofighi <ghassem+github@gmail.com>
2015-04-26 18:09:15 -0400

    First Commit After Editing Function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Matrix inversion is usually a costly computation and there may be some 
+#benefit to caching the inverse of a matrix rather than computing it repeatedly
+#In this way, the following functions will perform operations based on cashing
 
-## Write a short comment describing this function
+##This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(inputMatrix= matrix()) {
 
-makeCacheMatrix <- function(x = matrix()) {
-
+  invMatrix <- NULL
+  
+  setCached <- function(y) {
+    inputMatrix <<- y
+    invMatrix <<- NULL
+  }
+  
+  # Functions for getting and setting cached inverse matrix values
+  getCached <- function() inputMatrix
+  # Calculation inverse matrix using built-in solve() function
+  setCached.inverse <- function(solve) invMatrix <<- solve
+  getCached.inverse <- function() invMatrix
+  
+  list(
+    setCached = setCached, 
+    getCached = getCached,
+    setCached.inverse = setCached.inverse,
+    getCached.inverse = getCached.inverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then cacheSolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheable.matrix, ...) {
+  invMatrix <- cacheable.matrix$getCached.inverse()
+  # Checking if there is a cached matrix available
+  if(!is.null(invMatrix)) {
+    message("Getting cached inverse matrix")
+    return(invMatrix)
+  }
+
+  # If there is no cached matrix available, an inverted matrix will be created
+  matrix.to.inverse <- cacheable.matrix$getCached()
+  invMatrix <- solve(matrix.to.inverse)
+  cacheable.matrix$setCached.inverse(invMatrix)
+  # returning inverse
+  invMatrix
+  
 }
